1024ffe2499f3f0ad8e440c06b64db66c2862044
nicolaszs <1316236724@qq.com>
2017-11-27 08:03:54 +0800

    add insert sort

--- Sort/bubbleSort.py
+++ Sort/bubbleSort.py
@@ -1,13 +1,13 @@
-def bubbleSort (array) :
+def bubbleSort (sortList) :
     # 基线条件：为空或者只包含一个元素的数组是‘有序’的
-    if (len(array) < 2) :
-        return array
+    if (len(sortList) < 2) :
+        return sortList
 
     # 遍历 0,0-3，1,0-2，2,0-1
-    for i in range(len(array) - 1) :
-        for j in range(len(array) - i - 1) :
-            if array[j] > array[j + 1] :
-                array[j], array[j + 1] = array[j + 1], array[j]
-    return array
+    for i in range(len(sortList) - 1) :
+        for j in range(len(sortList) - i - 1) :
+            if sortList[j] > sortList[j + 1] :
+                sortList[j], sortList[j + 1] = sortList[j + 1], sortList[j]
+    return sortList
 
 print(bubbleSort([4, 2, 3, 6, 1]))
---
+++ Sort/insertSort.py
@@ -0,0 +1,12 @@
+def insertSort (sortList) :
+    if len(sortList) < 2 :
+        return sortList
+    for i in range(1, len(sortList)) :
+        # 当 list的前一个数字大于后一个数字，进行位置调换
+        # i - 1，循环判断前一个数是否大于后一个数
+        while i >= 1 and sortList[i - 1] > sortList[i] :
+            sortList[i], sortList[i - 1] = sortList[i - 1], sortList[i]
+            i -= 1
+    return sortList
+
+print(insertSort([3, 2, 5, 4, 6, 1]))
