3582d2ccb2f9acbc70952644208c2e0074645442
nicolaszs <1316236724@qq.com>
2017-11-27 08:36:11 +0800

    add search

--- Sort/selectionSort.py
+++ Sort/selectionSort.py
@@ -10,8 +10,8 @@ def selectionSort(sortList) :
         for j in range(i, len(sortList)) :
 
             if sortList[j] < smallest :
-                print(i, len(sortList))
-                print(j)
+                # print(i, len(sortList))
+                # print(j)
                 smallest = sortList[j]
                 location = j
 
@@ -21,3 +21,25 @@ def selectionSort(sortList) :
     return sortList
 
 print(selectionSort([2, 4, 6, 5, 1, 3]))
+
+# 存储最小元素的值
+# 存储最小元素的索引
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+# 对数组进行排序
+def selectSort(arr):
+    newArr = []
+    for j in range(len(arr)):
+        # 找出数组中最小的元素，并将其加入到新的数组中
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectSort([5, 3, 6, 2, 10]))
