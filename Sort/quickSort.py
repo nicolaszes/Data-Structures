3677b65a7ad861c85c748868652990bbf1f18e74
nicolaszs <1316236724@qq.com>
2017-12-01 00:19:37 +0800

    add quick sort in three ways

--- Sort/quickSort.py
+++ Sort/quickSort.py
@@ -1,17 +1,22 @@
 import math, random, datetime
 import arr as randomList
+# import insertSort as insert
 
 now = datetime.datetime.now()
 
-# 没有重复元素的数组 or 少量重复的数组
+'''
+基础快速排序
+没有重复元素的数组 or 少量重复的数组
+'''
 def quickSortNoRepeat(sortList) :
     # 基线条件：为空或者只包含一个元素的数组是‘有序’的
-    if len(sortList) < 2 :
-        return sortList
+    if len(sortList) < 2 : return sortList
 
-    # 递归条件
-    # 优化快速排序, 对于近似于有序的数组，使用随机快速排序
-    # 随机生成基准值，最后和数组的第一个值进行数据交换
+    '''
+    递归条件
+    优化快速排序, 对于近似于有序的数组，使用随机快速排序
+    随机生成基准值，最后和数组的第一个值进行数据交换
+    '''
     ran = int(math.floor(random.random() * len(sortList)))
     sortList[0], sortList[ran] = sortList[ran], sortList[0]
     pviot = sortList[0]
@@ -25,20 +30,23 @@ def quickSortNoRepeat(sortList) :
 quickSortNoRepeat(randomList.generateRandomList(1000, 0, 1000))
 
 now = datetime.datetime.now()
-
-def quickSort(sortList, left, right) :
+'''
+双路快速排序
+存在大量的重复元素的数组
+'''
+def quickSortInTwoWays(sortList, left, right) :
     # 基线条件：为空或者只包含一个元素的数组是‘有序’的
-    if len(sortList) < 2 :
+    if right - left < 2 :
         return sortList
-    index = partition(sortList, left, right)
+    index = partitionInTwoWays(sortList, left, right)
 
     # 递归条件
-    if index + 1 > 0 and index + 1 < right :
-        quickSort(sortList, left, index - 1)
-        quickSort(sortList, index + 1, right)
+    if index > 1 and index < right - 1 :
+        quickSortInTwoWays(sortList, left, index - 1)
+        quickSortInTwoWays(sortList, index + 1, right)
     return sortList
 
-def partition (arr, l, r) :
+def partitionInTwoWays (arr, l, r) :
     # 优化快速排序 => 随机化快速排序, 对于近似于有序的数组，使用随机快速排序
     # 随机生成基准值，最后和数组的第一个值进行数据交换
     ran = int(math.floor(random.random() * (r - l) + l))
@@ -47,6 +55,7 @@ def partition (arr, l, r) :
 
     i = l + 1
     j = r
+    # (i, j)这个区间
     while i <= j :
         while i <= r and arr[i] < pviot :
             i += 1
@@ -60,32 +69,81 @@ def partition (arr, l, r) :
     return j
 
 newList = randomList.generateRandomList(100000, 0, 10)
-# newList = [1, 2, 3, 2, 2, 3, 2, 4]
 left = 0
 right = len(newList) - 1
 
-print(len(quickSort(newList, left, right)))
-# print(quickSort(newList, left, right))
+print(len(quickSortInTwoWays(newList, left, right)))
 future = datetime.datetime.now()
-print(future - now)
+# 测试排序速度
+print('双路快速排序', future - now)
 
-# class quick_sort(sort_list):
-#     def _partition(self, alist, p, r):
-#         i = p-1
-#         x = alist[r]
-#         for j in range(p, r):
-#             if alist[j]<=x:
-#                 i += 1
-#                 alist[i], alist[j] = alist[j], alist[i]
-#         alist[i+1], alist[r] = alist[r], alist[i+1]
-#         return i+1
-#
-#     def _quicksort(self, alist, p, r):
-#         if p<r:
-#             q = self._partition(alist, p, r)
-#             self._quicksort(alist, p, q-1)
-#             self._quicksort(alist, q+1, r)
-#
-#     def __call__(self, sort_list):
-#         self._quicksort(sort_list, 0, len(sort_list)-1)
-#         return sort_list
+'''
+ 三路快速排序
+ 面向对象封装
+'''
+def quickSortInThreeWays (arr, l, r) :
+    # 基线条件：为空或者只包含一个元素的数组是‘有序’的
+    if r - l < 2 :
+        return arr
+
+    # partition
+    ran = int(math.floor(random.random() * (r - l) + l))
+    arr[l], arr[ran] = arr[ran], arr[l]
+    pviot = arr[l]
+
+    lt = l
+    gt = r + 1
+    i = l + 1
+
+    while i < gt :
+        if arr[i] < pviot :
+            arr[i], arr[lt + 1] = arr[lt + 1], arr[i]
+            lt += 1
+            i += 1
+        elif arr[i] > pviot :
+            arr[i], arr[gt - 1] = arr[gt - 1], arr[i]
+            gt -= 1
+        else :
+            i += 1
+    arr[l], arr[lt] = arr[lt], arr[l]
+
+    # 递归条件
+    quickSortInThreeWays(arr, l, lt - 1)
+    quickSortInThreeWays(arr, gt, r)
+    return arr
+
+newList = randomList.generateRandomList(100000, 0, 10)
+left = 0
+right = len(newList) - 1
+print(len(quickSortInThreeWays(newList, left, right)))
+
+ai = datetime.datetime.now()
+# 测试排序速度
+print('双路快速排序', ai - future)
+
+'''
+ 双路快速排序
+ 面向对象封装
+'''
+class quick_sort():
+    def _partition(self, alist, p, r):
+        i = p - 1
+        x = alist[r]
+        for j in range(p, r):
+            if alist[j] <= x:
+                i += 1
+                alist[i], alist[j] = alist[j], alist[i]
+        alist[i + 1], alist[r] = alist[r], alist[i+1]
+        return i + 1
+
+    def _quicksort(self, alist, p, r):
+        if p<r:
+            q = self._partition(alist, p, r)
+            self._quicksort(alist, p, q - 1)
+            self._quicksort(alist, q + 1, r)
+
+    def __call__(self, sort_list):
+        self._quicksort(sort_list, 0, len(sort_list) - 1)
+        return sort_list
+
+print(quick_sort)
