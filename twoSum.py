75d2018f81ad295f6f97e18b784723f5a507547a
nicolaszs <1316236724@qq.com>
2017-12-12 08:43:53 +0800

    beautify

--- twoSum.py
+++ twoSum.py
@@ -1,4 +1,4 @@
-"""
+'''
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -8,33 +8,33 @@ for example:
 
     Because nums[0] + nums[1] = 2 + 7 = 9,
     return [0, 1].
-"""
+'''
 def twoSum(self, nums, target):
-    """
+    '''
     :type nums: List[int]
     :type target: int
     :rtype: List[int]
-    """
+    '''
 
     if type(nums) != list:
         print('nums type should be a list')
-        return;
+        return
     if type(target) != int:
         print('target type should be a int')
         return
 
-    """
+    '''
     for loop the nums element
     get the number(n) equal to target minus m
-    """
+    '''
     for m in nums:
         n = target - m
 
-        """
+        '''
         # when nums have n, run the code below
         # if n equal to m and nums have more than one n element
         # if n not equal to m
-        """
+        '''
         if n in nums:
 
             if n == m and nums.count(n) > 1:
