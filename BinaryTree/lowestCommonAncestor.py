c9f210ce7bccea6bc059a16ce774e03e07bac770
nicolaszs <1316236724@qq.com>
2019-05-01 18:04:04 +0800

    add generate parenthesis

--- BinaryTree/lowestCommonAncestor.py
+++ BinaryTree/lowestCommonAncestor.py
@@ -9,19 +9,19 @@
 
 # 递归 O(N)
 class Solution:
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        if root in (None, p, q):
-            return root
-        
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        
-        if left == None: return right
-        elif right == None: return left
-        return root
+  def lowestCommonAncestor(self, root, p, q):
+    """
+    :type root: TreeNode
+    :type p: TreeNode
+    :type q: TreeNode
+    :rtype: TreeNode
+    """
+    if root in (None, p, q):
+      return root
+    
+    left = self.lowestCommonAncestor(root.left, p, q)
+    right = self.lowestCommonAncestor(root.right, p, q)
+    
+    if left == None: return right
+    elif right == None: return left
+    return root
